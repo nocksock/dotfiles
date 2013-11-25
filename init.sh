7bb11793717e6402afec38c225e9755aeb15b673
Nils Riedemann <nils.riedemann@gmail.com>
2013-11-25 17:19:54 +0100

    added git stuff to init

--- init.sh
+++ init.sh
@@ -6,3 +6,9 @@ ln -s dotfiles/dottmux/tmux.conf ~/.tmux.conf
 ln -s dotfiles/dotzsh/zsh ~/.zsh
 ln -s dotfiles/dotzsh/zshrc ~/.zshrc
 ln -s dotfiles/oh-my-zsh ~/.oh-my-zsh
+
+git submodule init;
+git submodule update;
+cd dotvim;
+git submodule init;
+git submodule update;
