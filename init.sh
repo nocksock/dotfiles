6e923ce3f9478a0e024627c21ac6aa5f85ac996a
Nils Riedemann <nils.riedemann@gmail.com>
2013-11-25 17:07:20 +0100

    added remove.sh and moar fixes

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ ln -s dotfiles/dotvim/vimrc ~/.vimrc
 ln -s dotfiles/dottmux/tmux.conf ~/.tmux.conf
 ln -s dotfiles/dotzsh/zsh ~/.zsh
 ln -s dotfiles/dotzsh/zshrc ~/.zshrc
-ln -s oh-my-zsh ~/.oh-my-zsh
+ln -s dotfiles/oh-my-zsh ~/.oh-my-zsh
---
+++ remove.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm ~/.vim
+rm ~/.vimrc
+rm ~/.tmux.conf
+rm ~/.zsh
+rm ~/.zshrc
+rm ~/.oh-my-zsh
