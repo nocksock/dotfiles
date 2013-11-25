73714beb5642ec8eeb0b1f6870c159d3d0138bad
Nils Riedemann <nils.riedemann@gmail.com>
2013-11-25 16:47:53 +0100

    Added init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s dotvim/vim ~/.vim
+ln -s dotvim/vimrc ~/.vimrc
+ln -s dottmux/tmux.conf ~/.tmux.conf
+ln -s dotzsh/zsh ~/.zsh
+ln -s dotzsh/zshrc ~/.zshrc
+ln -s oh-my-zsh ~/.oh-my-zsh
