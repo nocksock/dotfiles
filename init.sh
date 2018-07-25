7d59e3e8ef1b2a96094d6667b238d09737c59034
Nils Riedemann <riedemann@Nilss-MBP-2.fritz.box>
2018-07-25 09:21:17 +0200

    update

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ ln -s dotfiles/dotvim/vimrc ~/.vimrc
 ln -s dotfiles/dottmux/tmux.conf ~/.tmux.conf
 ln -s dotfiles/dotzsh/zsh ~/.zsh
 ln -s dotfiles/dotzsh/zshrc ~/.zshrc
-ln -s dotfiles/oh-my-zsh ~/.oh-my-zsh
+
 
 git submodule init;
 git submodule update;
