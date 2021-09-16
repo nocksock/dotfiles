e915ce35785941ee3ee02ba384176a564e08fcf6
Nils Riedemann <nils@bleepbloop.studio>
2021-09-16 12:56:56 +0200

    adjusting setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-ln -s .vim ~/.vim
-ln -s .vimrc ~/.vimrc
-ln -s .tmux.conf ~/.tmux.conf
-ln -s .zsh ~/.zsh
-ln -s .zshrc ~/.zshrc
+DOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "creating symlinks"
+
+ln -s $DOT_DIR/.vim ~/.vim
+ln -s $DOT_DIR/.vimrc ~/.vimrc
+ln -s $DOT_DIR/.tmux.conf ~/.tmux.conf
+ln -s $DOT_DIR/.zsh ~/.zsh
+ln -s $DOT_DIR/.zshrc ~/.zshrc
 
 git submodule init;
 git submodule update;
 
-mkdir ~/.vim/tmp;
-mkdir ~/.vim/tmp/undo;
-mkdir ~/.vim/tmp/swap;
-mkdir ~/.vim/tmp/backup;
+if [ -d "~/.vim/tmp" ]; then
+	echo "tmp folder already existings";
+	exit 1;
+else
+	mkdir ~/.vim/tmp;
+	mkdir ~/.vim/tmp/undo;
+	mkdir ~/.vim/tmp/swap;
+	mkdir ~/.vim/tmp/backup;
+fi
+
+echo "done";
