c0a936012cad0ae7d795a7f728aff96ae6b093c3
Nils Riedemann <nils@bleepbloop.studio>
2022-03-12 22:29:32 +0100

    refacter(init): use vim.o[...] directly
    setWith didn make it easier but also made the lsp loose track

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,90 +1,75 @@
 --
---  Hi!
---
---    This is my messy nvim configuration
+--  Hello there!
 --
 
-local function setWith(mod, options)
-	for key, value in pairs(options) do
-		mod[key] = value
-	end
-end
+vim.o.backup = true -- enable backups
+vim.o.backupskip = "/tmp/*,/private/tmp/*" -- Make Vim able to edit crontab files again.
+vim.o.breakindent = true -- wrapped lines appear indendet
+vim.o.clipboard = "unnamed"
+vim.o.completeopt = "menu,menuone,noselect"
+vim.o.cursorline = true -- Highlight the line of in which the cursor is present (or not)
+vim.o.expandtab = true -- use spaces for indentation by default
+vim.o.foldenable = true
+vim.o.formatoptions = "qrn1j" -- format options when writing, joining lines or `gq` see  :he fo-table for meanings
+vim.o.gdefault = true -- add g flag by default for :substitutions
+vim.o.ignorecase = true -- ignore case by default - unless using uppercase/lowercase via smartcase
+vim.o.list = true -- Show invisible characters
+vim.o.listchars = "tab:->,eol:¬,trail:-,extends:↩,precedes:↪" -- define characters for invisible characters
+vim.o.mouse = "a" -- enable scrolling and selecting with mouse
+vim.o.nu = true
+vim.o.rnu = true -- show *HYBRID* line numbers, relative line numbers + current line number
+vim.o.scrolloff = 2 -- always have 2 lines more visible when reaching top/end of a window when scrolling
+vim.o.shell = "/bin/zsh" -- set default shell for :shell
+vim.o.shiftround = true -- When at 3 spaces and I hit >>, go to 4, not 5.
+vim.o.shiftwidth = 2
+vim.o.showmatch = true -- Highlight matching bracket
+vim.o.signcolumn = "yes"
+vim.o.smartcase = true -- ignore 'ignorecase' when search contains uppercase characters
+vim.o.softtabstop = 2
+vim.o.splitbelow = true -- When on, splitting a window will put the new window below the current one
+vim.o.synmaxcol = 500 -- Until which column vim parses syntax
+vim.o.tabstop = 2
+vim.o.termguicolors = true -- enable 24bit colors
+vim.o.textwidth = 80
+vim.o.undofile = true
+vim.o.wrap = false -- don't wrap text around when the window is too small
+vim.o.wildignore = table.concat({
+	".DS_Store",
+	"**/.git/*",
+	"*.jpg,*.bmp,*.gif,*.png,*.jpeg", -- ignore binary images
+	"**/coverage/*",
+	"**/node_modules/*",
+	"**/android/*",
+	"**/.git/*",
+	"**/tmp/*",
+}, ",")
 
-setWith(vim.o, {
-	backup = true, -- enable backups
-	backupskip = "/tmp/*,/private/tmp/*", -- Make Vim able to edit crontab files again.
-	breakindent = true, -- wrapped lines appear indendet
-	clipboard = "unnamed",
-	completeopt = "menu,menuone,noselect",
-	cursorline = true, -- Highlight the line of in which the cursor is present (or not)
-	expandtab = true, -- use spaces for indentation by default
-	foldenable = true,
-	formatoptions = "qrn1j", -- format options when writing, joining lines or `gq` see  :he fo-table for meanings
-	gdefault = true, -- add g flag by default for :substitutions
-	ignorecase = true, -- ignore case by default - unless using uppercase/lowercase via smartcase
-	list = true, -- Show invisible characters
-	listchars = 'tab:->,eol:¬,trail:-,extends:↩,precedes:↪', -- define characters for invisible characters
-	mouse = "a", -- enable scrolling and selecting with mouse
-	nu = true,
-	rnu = true, -- show *HYBRID* line numbers, relative line numbers + current line number
-	scrolloff = 2, -- always have 2 lines more visible when reaching top/end of a window when scrolling
-	shell = "/bin/zsh", -- set default shell for :shell
-	shiftround = true, -- When at 3 spaces and I hit >>, go to 4, not 5.
-	shiftwidth = 2,
-	showmatch = true, -- Highlight matching bracket
-	signcolumn = "yes",
-	smartcase = true, -- ignore 'ignorecase' when search contains uppercase characters
-	softtabstop = 2,
-	splitbelow = true, -- When on, splitting a window will put the new window below the current one
-	synmaxcol = 500, -- Until which column vim parses syntax
-	tabstop = 2,
-	termguicolors = true, -- enable 24bit colors
-	textwidth = 80,
-	undofile = true,
-	wrap = false, -- don't wrap text around when the window is too small
-	wildignore = table.concat({
-		".DS_Store",
-		"**/.git/*",
-		"*.jpg,*.bmp,*.gif,*.png,*.jpeg", -- ignore binary images
-		"**/coverage/*",
-		"**/node_modules/*",
-		"**/android/*",
-		"**/.git/*",
-		"**/tmp/*",
-	}, ","),
-	wildmode="longest,list,full",
-	runtimepath=vim.o.runtimepath .. ",~/personal/ghash-vim"
-})
+vim.o.wildmode = "longest,list,full"
+vim.o.runtimepath = vim.o.runtimepath .. ",~/personal/ghash-vim"
 
-setWith(vim.g, {
-	mapleader = " ",
-	maplocalleader = "\\",
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
-	UltiSnipsExpandTrigger="<c-l>",
-	UltiSnipsJumpForwardTrigger="<c-l>",
-	UltiSnipsJumpBackwardTrigger="<c-h>",
+vim.g.UltiSnipsExpandTrigger = "<c-l>"
+vim.g.UltiSnipsJumpForwardTrigger = "<c-l>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<c-h>"
 
-	netrw_altfile=1, -- make CTRL-^ ignore netrw buffers
+vim.g.netrw_altfile = 1 -- make CTRL-^ ignore netrw buffers
 
-	db_ui_force_echo_notifications=1,
+vim.g.db_ui_force_echo_notifications = 1
 
-	closetag_filenames = '*.html,*.xhtml,*.phtml,*.js,*.jsx,*.tsx',
-	closetag_xhtml_filenames = '*.xhtml,*.jsx',
-	closetag_filetypes = 'html,xhtml,phtml,jsx,tsx,javascript',
-	closetag_xhtml_filetypes = 'xhtml,jsx,tsx',
-	closetag_emptyTags_caseSensitive = 1,
-	closetag_regions = {
-		['typescript.tsx'] = 'jsxRegion,tsxRegion',
-		['javascript.jsx'] = 'jsxRegion',
-	},
-	closetag_shortcut = '>',
-	closetag_close_shortcut = '<leader>>',
-})
+vim.g.closetag_filenames = "*.html*.xhtml*.phtml*.js*.jsx*.tsx"
+vim.g.closetag_xhtml_filenames = "*.xhtml*.jsx"
+vim.g.closetag_filetypes = "htmlxhtmlphtmljsxtsxjavascript"
+vim.g.closetag_xhtml_filetypes = "xhtmljsxtsx"
+vim.g.closetag_emptyTags_caseSensitive = 1
+vim.g.closetag_regions = {
+	["typescript.tsx"] = "jsxRegion,tsxRegion",
+	["javascript.jsx"] = "jsxRegion",
+}
+vim.g.closetag_close_shortcut = "<leader>>"
 
-
--- TODO: convert to actual lua
 vim.cmd([[
-	" enable filtype plugins
 	filetype plugin on
 
 	" When saving a buffer, create directories if they do not yet exist.
@@ -93,4 +78,3 @@ vim.cmd([[
 		autocmd BufWritePre * call mkdir(expand("<afile>:p:h"), "p")
 	augroup END
 ]])
-
