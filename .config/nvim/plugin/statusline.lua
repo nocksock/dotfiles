186a3cadf80fc6ff811cfa5f1b005732d2c7fe63
Nils Riedemann <nils@bleepbloop.studio>
2022-03-12 21:35:20 +0100

    refactor(statusline): show current search + lsp status
    and simplifying things a lot too.

--- .config/nvim/plugin/statusline.lua
+++ .config/nvim/plugin/statusline.lua
@@ -1,96 +1,58 @@
 -- statusline
 -- a super simple, custom status bar written in lua*
 
-local flaggedFile = '%(%m%r%h %)%f'
-local flex = ' %='
-local bar = '|'
-local filetype = '%y '
-
--- creates a function that appends the context to the groupName
---    eg. createContext("Active")("Foo") -> "FooActive"
-local function createContext(context, wrap)
+local function color_state(state, wrap)
 	local function cg(name)
 		return '%#' .. name .. '#'
 	end
 
-	return function(groupName)
+	return function(name)
 		if wrap == nil or wrap == true then
-			return cg(groupName .. context)
+			return cg(name .. state)
 		end
 
-		return groupName .. context
+		return name .. state
 	end
 end
 
--- local function system(cmd)
---   local handle = io.popen(cmd)
---   local result = handle:read("*a")
---   handle:close()
---   return result
--- end
-
-local get_git_status = function()
-	-- TODO: check if git is even present
-	return ''
-	-- return system('git status -s | cut -c1-2 | uniq | sed "s/??/+/" | tr "\n" " " | sed "s/ //g" | tr "[:upper:]" "[:lower:]"')
+local lsp_status = function()
+	return vim.lsp.buf.server_ready() and "+lsp" or ""
 end
 
-local get_git_branch = function()
-	-- TODO: check if git is even present
-	return ''
-	-- return system('git branch --show-current | tr "\n" " "'):gsub(" ","");
-end
-
-local get_mode = function()
+local mode = function()
 	return vim.api.nvim_get_mode().mode:upper()
 end
 
-local stl = function(group, separator)
-	if separator == nil then
-		separator = ' '
-	end
-
+local stl = function(color)
 	return table.concat({
-		group('stlModeMsg'),
-		get_mode(),
-		group('stlLeft'),
-		flaggedFile,
-		group('stlMid'),
-		get_git_branch(),
-		bar,
-		get_git_status(),
-		flex,
-		group('stlRight'),
-		filetype,
-	}, separator)
+		color('stlModeMsg'),
+		mode(),
+		color('stlLeft'),
+		'%(%m%r%h %)%f',
+		color('stlMid'),
+		"%{get(b:,'gitsigns_head','')}",
+		"%{get(b:,'gitsigns_status','')}",
+		'%=',
+		"/%{@/}",
+		color('stlRight'),
+		lsp_status(),
+		'%y',
+	}, " ")
 end
 
 function StatusLine(type)
 	if type == 'active' then
-		return stl(createContext(''))
+		return stl(color_state(''))
 	end
-
 	if type == 'inactive' then
-		return stl(createContext('NC'))
+		return stl(color_state('NC'))
 	end
 end
 
--- TODO: when updating to new neovim version, rewrite this using lua
-vim.api.nvim_exec(
-	[[
-  highlight! link stlModeMsg ModeMsg
-  highlight! link stlLeftNC StatusLineNC
-  highlight! link stlMidNC StatusLineNC
-  highlight! link stlRightNC StatusLineNC
-  highlight! link stlLeft StatusLine
-  highlight! link stlMid StatusLine
-  highlight! link stlRight StatusLine
-
+vim.api.nvim_exec([[
   augroup statusline
     au!
     autocmd WinEnter,BufEnter * setlocal statusline=%!v:lua.StatusLine('active')
     autocmd WinLeave,BufLeave * setlocal statusline=%!v:lua.StatusLine('inactive')
   augroup END
-]],
-	false
-)
+]], false)
