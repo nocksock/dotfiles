3993fd27f8d23ccc4bf88d7c6488c1fce70fd493
Nils Riedemann <nils@bleepbloop.studio>
2022-03-12 21:53:33 +0100

    refactor: delete settings that match nvims defaults

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,51 +10,38 @@ local function setWith(mod, options)
 	end
 end
 
-
 setWith(vim.o, {
-	autoindent = true, -- Copy indent from current line when creating a new line                            --
-	autoread = true, -- auto re-read file when it changed outside of vim, but not inside
 	backup = true, -- enable backups
-	breakindent = true, -- wrapped lines appear indendet
-	expandtab = true, -- use spaces for indentation by default
-	foldenable = true,
-	gdefault = true, -- add g flag by default for :substitutions
-	hidden = true, -- enable hidden buffers - so i can switch buffers even if current is changed.
-	hlsearch = true, -- highlight search results
-	incsearch = true, -- enable incremental search that would make vim jump around while typing
-	ignorecase = true, -- ignore case by default - unless using uppercase/lowercase via smartcase
-	list = true, -- Show invisible characters
-	cursorline = true, -- Highlight the line of in which the cursor is present (or not)
-	wrap = false, -- don't wrap text around when the window is too small
-	nu = true,
-	rnu = true, -- show *HYBRID* line numbers, relative line numbers + current line number
-	shiftround = true, -- When at 3 spaces and I hit >>, go to 4, not 5.
-	showcmd = true, -- display incomplete commands
-	showmatch = true, -- Highlight matching bracket
-	smartcase = true, -- ignore 'ignorecase' when search contains uppercase characters
-	splitbelow = true, -- When on, splitting a window will put the new window below the current one
-	termguicolors = true, -- enable 24bit colors
-	undofile = true,
-	wildmenu = true,
-	backspace = "indent,eol,start", -- allow backspacing over everything in insert mode
-	backupdir = "~/.config/nvim/tmp/backup/", -- backups
 	backupskip = "/tmp/*,/private/tmp/*", -- Make Vim able to edit crontab files again.
+	breakindent = true, -- wrapped lines appear indendet
 	clipboard = "unnamed",
 	completeopt = "menu,menuone,noselect",
-	encoding = "utf-8",
+	cursorline = true, -- Highlight the line of in which the cursor is present (or not)
+	expandtab = true, -- use spaces for indentation by default
+	foldenable = true,
 	formatoptions = "qrn1j", -- format options when writing, joining lines or `gq` see  :he fo-table for meanings
-	signcolumn = "yes",
-	history = 10000, -- keep way more commands in history
+	gdefault = true, -- add g flag by default for :substitutions
+	ignorecase = true, -- ignore case by default - unless using uppercase/lowercase via smartcase
+	list = true, -- Show invisible characters
 	listchars = 'tab:->,eol:¬,trail:-,extends:↩,precedes:↪', -- define characters for invisible characters
 	mouse = "a", -- enable scrolling and selecting with mouse
+	nu = true,
+	rnu = true, -- show *HYBRID* line numbers, relative line numbers + current line number
 	scrolloff = 2, -- always have 2 lines more visible when reaching top/end of a window when scrolling
 	shell = "/bin/zsh", -- set default shell for :shell
+	shiftround = true, -- When at 3 spaces and I hit >>, go to 4, not 5.
 	shiftwidth = 2,
+	showmatch = true, -- Highlight matching bracket
+	signcolumn = "yes",
+	smartcase = true, -- ignore 'ignorecase' when search contains uppercase characters
 	softtabstop = 2,
+	splitbelow = true, -- When on, splitting a window will put the new window below the current one
 	synmaxcol = 500, -- Until which column vim parses syntax
 	tabstop = 2,
+	termguicolors = true, -- enable 24bit colors
 	textwidth = 80,
-	undodir = "~/.config/nvim/tmp/undo/",
+	undofile = true,
+	wrap = false, -- don't wrap text around when the window is too small
 	wildignore = table.concat({
 		".DS_Store",
 		"**/.git/*",
