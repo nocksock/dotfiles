2d53daec9b859d7db2b47c4439b78578cb4bc7a9
Nils Riedemann <nils@bleepbloop.studio>
2022-06-02 16:18:00 +0200

    feat(hammerspoon): add hammerspoon to dotfiles

---
+++ hammerspoon/applauncher.lua
@@ -0,0 +1,40 @@
+local apps = hs.hotkey.modal.new({ "cmd", "control", "shift", "option" }, "o")
+
+function apps:entered()
+	hs.alert.show("press key")
+end
+
+function apps:exited()
+	hs.alert.closeAll()
+end
+
+apps:bind("", "escape", function()
+	apps:exit()
+end)
+
+local function launch(name)
+	return function()
+		local app = hs.application.find(name)
+
+		if not app or app:isHidden() then
+			hs.application.launchOrFocus(name)
+		elseif app:isFrontmost() then
+			app:hide()
+		else
+			app:activate()
+		end
+
+		apps:exit()
+	end
+end
+
+apps:bind("", "t", "Kitty", launch("kitty"))
+apps:bind("", "v", "Visual Studio Code", launch("Visual Studio Code"))
+apps:bind("", "c", "Chrome", launch("Google Chrome"))
+apps:bind("", "f", "Firefox", launch("Firefox"))
+apps:bind("", "s", "Safari", launch("Safari"))
+apps:bind("", "p", "Spotify", launch("Spotify"))
+apps:bind("", "g", "Things", launch("Things3"))
+apps:bind("", "n", "Obsidian", launch("Obsidian"))
+apps:bind("", "m", "Mail", launch("Mail"))
+apps:bind("", "o", "Discord", launch("Discord"))
---
+++ hammerspoon/type.lua
@@ -0,0 +1,1 @@
+hs = require('hammerspoon')
