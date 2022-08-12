f67bb6b515d17494c67bbbdbaff857029d1c1fac
Nils Riedemann <nils@bleepbloop.studio>
2022-08-12 17:37:41 +0200

    nvim/fix(on-save): fix wrong command call

--- nvim/plugin/on-save/init.lua
+++ nvim/plugin/on-save/init.lua
@@ -115,7 +115,7 @@ vim.api.nvim_create_user_command("OnSave", function(opts)
           end
         })
       else
-        vim.cmd(table.concat(command, " "))
+        vim.cmd(command, " ")
       end
     end)
   })
