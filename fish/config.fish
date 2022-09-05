321fd7a6647207238bab1d00b0f53d48ab245956
Nils Riedemann <nils@bleepbloop.studio>
2022-09-05 11:01:59 +0200

    fish/chore: remove obsolete paths from $PATH
    and move $DOTDIR/bin to the front.

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,19 @@
+# {{{
 if status --is-login
   set -g DOTDIR "$HOME/code/dotfiles"
   fish_add_path "/usr/local/bin:$PATH" # some apps put there stuff here (eg vscode, mullvad)
   fish_add_path "/opt/homebrew/bin:$PATH"
   fish_add_path "$HOME/bin:$PATH"
   fish_add_path "$HOME/go/bin:$PATH"
-  fish_add_path "$HOME/bin/gh/bin:$PATH"
   fish_add_path "$HOME/.composer/vendor/bin:$PATH"
   fish_add_path "$HOME/.emacs.d/bin:$PATH"
   fish_add_path "$HOME/.poetry/bin:$PATH"
-  fish_add_path "/opt/homebrew/opt/openjdk@11/bin:$PATH"
-  fish_add_path "$DOTDIR/bin/:$PATH"
   fish_add_path "$PYENV_ROOT/bin/:$PATH"
   fish_add_path "/usr/local/cuda-11.6/bin:$PATH"
   fish_add_path "$BUN_INSTALL/bin:$PATH"
+  fish_add_path "$DOTDIR/bin/:$PATH"
 end
+# }}}
 
 set -g __fish_git_prompt_show_informative_status true
 set -g __fish_git_prompt_showupstream auto
@@ -51,3 +51,4 @@ alias glog='git log --oneline --decorate --graph'
 alias gloga='git log --oneline --decorate --graph --all'
 alias glp="_git_log_prettily"
 
+# vi: fen fdm=marker fdl=0
