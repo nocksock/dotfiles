c94ad68a6b62a5a278e386f6d26d21fe1a145fa8
Nils Riedemann <nils@bleepbloop.studio>
2022-10-10 22:46:31 +0200

    sketchybar(front_app,spaces): styling

--- sketchybar/plugins/front_app.sh
+++ sketchybar/plugins/front_app.sh
@@ -8,6 +8,10 @@ if [[ $1 == "setup" ]]; then
     --add item $NAME $POSITION          \
     --set $NAME                         \
       script="$PLUGIN_DIR/front_app.sh" \
+      label.padding_left=$SPACE_2       \
+      label.padding_right=$SPACE_2      \
+      background.color=$BLACK_ALPHA     \
+      background.padding_left=32        \
       label.color=$COLOR_ACTIVE         \
       icon.drawing=off                  \
     --subscribe front_app front_app_switched
--- sketchybar/plugins/spaces.sh
+++ sketchybar/plugins/spaces.sh
@@ -4,11 +4,12 @@ source $HOME/.config/sketchybar/vars.sh
 update() {
   if [[ $SELECTED == true ]]; then
 
-    sketchybar                      \
-      --set $1                      \
-        icon.color=$BLACK           \
-        label.color=$BLACK          \
-        background.color=$COLOR_ACTIVE
+    sketchybar                         \
+      --set $1                         \
+        icon.color=$BLACK              \
+        label.color=$BLACK             \
+        background.color=$COLOR_ACTIVE \
+        background.drawing=on
 
   else
 
@@ -16,7 +17,7 @@ update() {
       --set $1                      \
         icon.color=$COLOR_INACTIVE  \
         label.color=$COLOR_INACTIVE \
-        background.color=$BLACK_ALPHA
+        background.drawing=off
 
   fi
 }
@@ -28,32 +29,45 @@ if [[ $1 == "setup" ]]; then
   for i in "${!SPACE_ICONS[@]}"; do
 
     sid=$(($i+1))
-    echo $sid
-    sketchybar                         \
-      --add space space.$sid $POSITION \
-      --set space.$sid                 \
-        associated_space=$sid          \
-        icon=${SPACE_ICONS[i]}         \
-        icon.padding_left=8            \
-        icon.padding_right=4           \
-        background.padding_right=8     \
-        background.corner_radius=5     \
-        background.height=24           \
-        background.drawing=on     \
-        label.drawing=on               \
-        label.padding_right=8          \
-        label=${SPACE_LABELS[i]}       \
-        script="$PLUGIN_DIR/spaces.sh" \
+    case "$sid" in
+      1) margin_left=0 ;;
+      *) margin_left=$SPACE_2 ;;
+    esac
+
+    echo $margin_left
+
+    sketchybar                                          \
+      --add space space.$sid $POSITION                  \
+      --set space.$sid                                  \
+        associated_space=$sid                           \
+        icon=${SPACE_ICONS[i]}                          \
+        icon.padding_left=8                             \
+        icon.padding_right=4                            \
+        background.padding_right=0                      \
+        background.padding_left=$margin_left            \
+        background.corner_radius=5                      \
+        background.height=24                            \
+        background.drawing=off                          \
+        label.drawing=on                                \
+        label.padding_right=8                           \
+        label=${SPACE_LABELS[i]}                        \
+        script="$PLUGIN_DIR/spaces.sh"                  \
         click_script="$PLUGIN_DIR/spaces.sh click $sid" \
 
     update space.$sid
   done
 
-  # sketchybar \
-  #   --add bracket primary_spaces space.SIDE space.MAIN space.COMM space.PLAN \
-  #   --set primary_spaces \
-  #     background.color=$GREY \
-  #     background.corner_radius=5
+  sketchybar                        \
+    --add bracket primary_spaces    \
+      space.1                       \
+      space.2                       \
+      space.3                       \
+      space.4                       \
+      space.5                       \
+      space.6                       \
+    --set primary_spaces            \
+      background.color=$BLACK_ALPHA \
+      background.corner_radius=5
 
   exit
 fi
