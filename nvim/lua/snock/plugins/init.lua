2c1dd4ea7fffd89af86dd0e7d1fcc6cc373c88e1
Nils Riedemann <nils@bleepbloop.studio>
2022-10-19 21:51:26 +0200

    nvim/feat: move lualine config to file + changes
    removed location, moved branch to the right, no filename etc

--- nvim/lua/snock/plugins/init.lua
+++ nvim/lua/snock/plugins/init.lua
@@ -151,49 +151,7 @@ require('packer').startup({ function(use)
   end }) -- }}}
   -- lualine.nvim: easy to configure and extend statusline (+tabline, +windowline) {{{
   use({ 'nvim-lualine/lualine.nvim', config = function()
-    require('lualine').setup {
-      options = {
-        icons_enabled = false,
-        theme = 'auto',
-        component_separators = '',
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {},
-      },
-      sections = {
-        lualine_a = { { 'mode' } },
-        lualine_b = { { 'branch' }, 'diff', 'diagnostics' },
-        lualine_c = { { 'filename', path = 1 } },
-        lualine_x = { 'location' },
-        lualine_y = { {
-          'filetype',
-          fmt = function(str)
-            return vim.lsp.buf.server_ready() and str .. "+" or str
-          end,
-          icons_enabled = true
-        } },
-        lualine_z = {}
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { { 'filename' } },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {}
-      },
-      tabline = {
-        lualine_a = { { require("snock.plugins.harpoon-lualine") } },
-        lualine_z = { { 'tabs', mode = 0 } },
-      },
-      -- currently using the implementation of do.nvim
-      -- winbar = {
-      --   -- lualine_a = { require("do").view },
-      -- },
-      -- inactive_winbar = {
-      --   -- lualine_a = { require("do").view_inactive }
-      -- },
-      extensions = {},
-    }
+    require "snock.plugins.lualine"
   end }) -- }}}
   use('https://github.com/kyazdani42/nvim-web-devicons') -- bunch of icons for web development
   -- fidget.nvim: ui for nvm-lsp progress {{{
