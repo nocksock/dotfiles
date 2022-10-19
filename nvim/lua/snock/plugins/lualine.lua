61471f372ae7f86941f8e6fc490a24347b42c014
Nils Riedemann <nils@bleepbloop.studio>
2022-10-19 22:17:34 +0200

    nvim/feat(lualine): show only filename instead of path
    let's see how this feels. there's always c-g to quickly get more info

--- nvim/lua/snock/plugins/lualine.lua
+++ nvim/lua/snock/plugins/lualine.lua
@@ -13,7 +13,6 @@ require('lualine').setup {
       {
         'filename',
         newfile_status = true,
-        path = 1,
         symbols = {
           modified = '*',
           readonly = '',
@@ -29,7 +28,7 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { { 'filename', path = 1 } },
+    lualine_c = { { 'filename' } },
     lualine_x = {},
     lualine_y = {},
     lualine_z = {}
