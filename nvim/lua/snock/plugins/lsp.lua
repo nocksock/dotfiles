fca3b72c5a27d73299d54c4b6ec292ec2a1a81cf
Nils Riedemann <nils@bleepbloop.studio>
2022-10-19 17:53:21 +0200

    nvim/fix: adjust to cmp deprecation warning

--- nvim/lua/snock/plugins/lsp.lua
+++ nvim/lua/snock/plugins/lsp.lua
@@ -11,7 +11,7 @@ local builtin = require('telescope.builtin')
 
 local servers = { 'clangd', 'tsserver', 'sumneko_lua', 'gopls', 'bashls' }
 local manual_servers = { 'tsserver', 'sumneko_lua' }
-local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require("mason-lspconfig").setup({ ensure_installed = servers })
 
