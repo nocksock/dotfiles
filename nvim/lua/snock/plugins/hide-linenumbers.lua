551ef0555210b5249410cb9c8b42e81e0a37fb83
Nils Riedemann <nils@bleepbloop.studio>
2022-10-27 17:39:53 +0200

    feat(nvim/hide-linenumbers): rewrite :Num etc

--- nvim/lua/snock/plugins/hide-linenumbers.lua
+++ nvim/lua/snock/plugins/hide-linenumbers.lua
@@ -1,21 +1,45 @@
--- :LineNrToggle
--- -------------
--- 
--- Hide line numbers without jumping or changing rnu nu settings. Results in
--- a padding that feels nice in minimal modes.
-
-local function is_hidden()--{{{
-  local get = vim.api.nvim_get_hl_by_name
-  return get("Normal", true).background == get("LineNr", true).foreground
-end--}}}
-local function toggle()--{{{
-  if is_hidden() then
-    print("showing LineNr")
-    vim.cmd(string.format([[ set background=%s ]], vim.o.background)) -- reloads colorscheme
-  else
-    print("hiding LineNr")
-    vim.api.nvim_set_hl(0, "LineNr", { fg = "bg" })
+---iterate over all relevant windows
+---@param cb function(window: number)
+local function map_win(cb)
+  for _, value in ipairs(vim.api.nvim_list_wins()) do
+    if vim.fn.win_gettype() == "" then
+      cb(value)
+    end
   end
-end--}}}
+end
 
-vim.api.nvim_create_user_command("LineNrToggle", toggle, {})
+---@return boolean
+local function numbers_visible()
+  for _, value in ipairs(vim.api.nvim_list_wins()) do
+    if vim.fn.win_gettype() == "" then
+      return vim.api.nvim_win_get_option(value, "rnu")
+    end
+  end
+
+  error("no windows found", 1)
+end
+
+local function show()
+  map_win(function(value)
+    vim.api.nvim_win_set_option(value, "rnu", true)
+    vim.api.nvim_win_set_option(value, "nu", true)
+  end)
+end
+
+local function hide()
+  map_win(function(value)
+    vim.api.nvim_win_set_option(value, "rnu", false)
+    vim.api.nvim_win_set_option(value, "nu", false)
+  end)
+end
+
+vim.api.nvim_create_user_command("NumShow", show, {})
+vim.api.nvim_create_user_command("NumHide", hide, {})
+
+vim.api.nvim_create_user_command("Num", function()
+  if numbers_visible() then
+    hide()
+  else
+    show()
+  end
+end, {})
