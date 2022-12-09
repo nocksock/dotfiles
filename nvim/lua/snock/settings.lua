8e42d69901c7b12425b71ccc69b3dd4785cca095
Nils Riedemann <nils@bleepbloop.studio>
2022-12-09 23:46:05 +0100

    feat(nvim): enable smartindent

--- nvim/lua/snock/settings.lua
+++ nvim/lua/snock/settings.lua
@@ -59,6 +59,7 @@ vim.g.symbols_outline = {
 -- indentation and wrapping{{{
 vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
+vim.o.smartindent = true
 vim.o.tabstop = 2
 vim.o.textwidth = 80
 vim.o.expandtab = true
