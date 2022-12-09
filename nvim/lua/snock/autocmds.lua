12f0e961136a6103f972412b5bd8de36735e3991
Nils Riedemann <nils@bleepbloop.studio>
2022-12-09 13:17:08 +0100

    feat(nvim): load autocommands
    no wondedr they werent workin

--- nvim/lua/snock/autocmds.lua
+++ nvim/lua/snock/autocmds.lua
@@ -1,15 +1,6 @@
 -- global auto commands 
 local group = vim.api.nvim_create_augroup('snock', { clear = true })
 
-vim.api.nvim_create_autocmd('BufAdd', { --{{{
-  callback = function()
-    -- lazy load lsp and git
-    -- require("snock.lsp")
-    -- require("snock.git")
-    -- require("snock.completion")
-  end,
-  group = group,
-}) --}}}
 vim.api.nvim_create_autocmd('insertenter', { --{{{
   callback = function()
     if vim.o.nu then
@@ -19,13 +10,16 @@ vim.api.nvim_create_autocmd('insertenter', { --{{{
   end,
   group = group,
 }) --}}}
+
 vim.api.nvim_create_autocmd('TermOpen', { --{{{
   callback = function()
     vim.bo.filetype = "terminal"
+    print "termopen"
     vim.keymap.set('n', '<C-c>', 'i<C-c>', { buffer = true })
   end,
   group = group
 }) --}}}
+
 vim.api.nvim_create_autocmd('insertleave', { --{{{
   callback = function()
     if vim.o.nu then
@@ -35,6 +29,7 @@ vim.api.nvim_create_autocmd('insertleave', { --{{{
   end,
   group = group,
 }) --}}}
+
 vim.api.nvim_create_autocmd('TextYankPost', { --{{{
   callback = function()
     vim.highlight.on_yank()
