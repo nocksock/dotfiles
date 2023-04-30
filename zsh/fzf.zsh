0fbd9185b8ec4732017af5892c1239daf64b2752
Nils Riedemann <nils@bleepbloop.studio>
2023-04-30 02:22:16 +0200

    feat(zsh/fzf): load key bindings + completion, check if installed

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,4 +1,13 @@
+# check if fzf is installed
+if ! command -v fzf &> /dev/null; then
+    style -fg yellow "Warning: fzf wast not found"
+    return
+fi
+
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # changing the default command to ignore vcs, git, node_modules etcp
 export FZF_DEFAULT_COMMAND='rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,node_modules.*/,.git/*,package-lock.json}" '
 export FZF_DEFAUTLT_OPTS='--height 40% --layout=reverse --border --bind up:preview-up,down:preview-down'
+
+source /opt/homebrew/opt/fzf/shell/completion.zsh
+source /opt/homebrew/opt/fzf/shell/key-bindings.zsh
