3ea3f5b4d1885ef785ee21cf1758d10f3b1b40bd
Nils Riedemann <nils@bleepbloop.studio>
2023-05-26 18:57:08 +0200

    feat(zsh/nvm): disable all nvm
    I rarely switch node version and got annoyed by the performance hit etc

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,14 +1,6 @@
-export NVM_NO_USE=true
-export NVM_LOADED=false
+# export NVM_NO_USE=true
+# export NVM_LAZY_LOAD_EXTRA_COMMANDS=('nvim')
+# export NVM_LAZY_LOAD=true
 
-function nvm() {
-  if [ "$NVM_LOADED" = false ]; then
-    if [ -s $HOME/.nvm/nvm.sh ]; then
-      . $HOME/.nvm/nvm.sh # This loads NVM
-      NVM_LOADED=true
-    fi
-  fi
-
-  nvm "$@"
-}
-
+# zgen load lukechilds/zsh-nvm
+# complete -o nospace -C /opt/homebrew/bin/terraform terraform
