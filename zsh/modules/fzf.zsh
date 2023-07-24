b1bb23598963b51a89877fcbab9857955dfa57e1
Nils Riedemann <nils@bleepbloop.studio>
2023-07-24 10:52:46 +0200

    feat(fzf): change default opts for fzf
    and make them more easily manageable

--- zsh/modules/fzf.zsh
+++ zsh/modules/fzf.zsh
@@ -1,13 +1,22 @@
 # check if fzf is installed
 if ! command -v fzf &> /dev/null; then
-    style -fg yellow "Warning: fzf wast not found"
+    style -fg yellow "Warning: fzf was not found!"
     return
 fi
 
+export FZF_OPTS=(
+  "-m"
+  "--bind ctrl-j:down,ctrl-k:up"
+  "--bind ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up"
+  "--bind ctrl-a:select-all,ctrl-t:toggle-all"
+  "--bind ctrl-s:toggle-sort,ctrl-r:toggle-preview,ctrl-/:toggle-search"
+)
+
+export FZF_DEFAULT_COMMAND='rg --files --follow --hidden'
+export FZF_DEFAULT_OPTS="$FZF_OPTS"
+
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-# changing the default command to ignore vcs, git, node_modules etcp
-export FZF_DEFAULT_COMMAND='rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,node_modules.*/,.git/*,package-lock.json}" '
-export FZF_DEFAULT_OPTS='-m --bind shift-up:preview-up,shift-down:preview-down,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-t:toggle-all'
+export FZF_CTRL_T_OPTS="--preview 'bat -pf --line-range :999 {}' --bind='ctrl-o:execute(nvim {})'"
 
 if [[ $OSTYPE == darwin* ]]; then
   test -f /opt/homebrew/opt/fzf/shell/completion.zsh   \
@@ -22,3 +31,4 @@ if [[ $OSTYPE == linux-gnu ]]; then
   test -f /usr/share/doc/fzf/examples/key-bindings.zsh \
     && source /usr/share/doc/fzf/examples/key-bindings.zsh
 fi
+
