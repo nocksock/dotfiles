13395384d5d97282ff5b7394fc88e435898f4b8d
Nils Riedemann <nils@bleepbloop.studio>
2023-07-31 00:32:35 +0200

    feat(nvim): even simpler lualine

--- nvim/lua/configs/lualine.lua
+++ nvim/lua/configs/lualine.lua
@@ -1,42 +1,45 @@
+
+local symbols = {
+  modified = '%#ErrorMsg#*',
+  readonly = '  ',
+  unnamed  = ' [No Name]',
+  newfile  = ' %#WarningMsg# new',
+}
 require('lualine').setup {
   options = {
     icons_enabled = false,
     theme = 'auto',
     component_separators = '',
     section_separators = { left = '', right = '' },
-    disabled_filetypes = {},
+    disabled_filetypes = { },
   },
   sections = {
-    lualine_a = { { 'mode' } },
-    lualine_b = { { 'tabs', mode = 0, cond = function() return vim.fn.tabpagenr('$') > 1 end } },
+    lualine_a = { },
+    lualine_b = {
+      { 'tabs', mode = 0, cond = function() return vim.fn.tabpagenr('$') > 1 end },
+    },
     lualine_c = {
-      { 'branch', padding = { left = 1, right = 0 }, separator = "", color = { fg = 8 } },
       {
         'filename',
         newfile_status = true,
         path = 1,
-        symbols = {
-          modified = '*',
-          readonly = '',
-          unnamed  = '[No Name]',
-          newfile  = ' new',
-        }
+        symbols = symbols
       }
     },
     lualine_x = { 'diagnostics' },
     lualine_y = { 'searchcount' },
-    lualine_z = {}
+    lualine_z = { }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { { 'filename', path = 1 } },
+    lualine_c = { { 'filename', path = 0, newfile_status = true, symbols = symbols } },
     lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {},
-  winbar = {},
+  tabline = { },
+  winbar = { },
   inactive_winbar = { },
   extensions = {},
 }
