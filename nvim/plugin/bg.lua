2bc478016b2a15b11b1d0a9d585eca0704d58885
Nils Riedemann <nils@bleepbloop.studio>
2023-09-03 23:41:13 +0200

    chore(nvim): delete unused functions.

--- nvim/plugin/bg.lua
+++ nvim/plugin/bg.lua
@@ -1,3 +1,15 @@
+function index_of(tbl, value) --{{{
+  for idx, tval in ipairs(tbl) do
+    if value == tval then
+      return idx
+    end
+  end
+end --}}}
+function cycle(values, value) --{{{
+  local next_val_idx = ((M.index_of(values, value)) % #values) + 1
+  return values[next_val_idx]
+end --}}}
+
 vim.api.nvim_create_user_command("BG", function()
-  vim.o.background = R("snock.utils").cycle({"dark", "light"}, vim.o.background)
+  vim.o.background = cycle({"dark", "light"}, vim.o.background)
 end, {})
