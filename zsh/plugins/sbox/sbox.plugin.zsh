98f0c4019d9d94f2c7cf42b683eaee915e15fbcb
Nils Riedemann <nils@bleepbloop.studio>
2023-10-01 13:39:48 +0200

    refactor(sbox): decouple builders into templates, simplify logic

--- zsh/plugins/sbox/sbox.plugin.zsh
+++ zsh/plugins/sbox/sbox.plugin.zsh
@@ -2,6 +2,6 @@
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 [[ -z ${fpath[(r)${0:h}/functions]} ]] && fpath+=( "${0:h}/functions" )
-# [[ -z ${path[(r)${0:h}/bin]} ]] && path+=( "${0:h}/bin" )
+[[ -z ${path[(r)${0:h}/bin]} ]] && path+=( "${0:h}/bin" )
 
 autoload -Uz ${0:h}/functions/*(:t)
