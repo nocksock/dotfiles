3d198d2dc14d26ee880645a5d2f2c24837744f00
Nils Riedemann <nils@bleepbloop.studio>
2024-02-24 12:13:55 +0100

    feat(nvim): add more configurations to treesj

--- nvim/plugin/treesj.lua
+++ nvim/plugin/treesj.lua
@@ -1,4 +1,6 @@
-local setup = require "baggage".from 'https://github.com/Wansmer/treesj'
+require "baggage".from 'https://github.com/Wansmer/treesj'
+
+vim.keymap.set({ "n" }, "m", function() require 'treesj'.toggle() end)
 
 local attribute_toggle = function(node_type, parent_type)
   return {
@@ -28,7 +30,7 @@ local attribute_toggle = function(node_type, parent_type)
   }
 end
 
-require'treesj'.setup {
+require 'treesj'.setup {
   use_default_keymaps = false,
   max_join_length = 200,
   cursor_behavior = 'start',
@@ -37,22 +39,23 @@ require'treesj'.setup {
     elixir = {
       ['quoted_attribute_value'] = attribute_toggle('attribute_value', 'attribute')
     },
-    heex = {
+    php   = {
+      ['attribute_value'] = attribute_toggle('attribute_value', 'quoted_attribute_value')
+    },
+    heex   = {
       ['quoted_attribute_value'] = attribute_toggle('attribute_value', 'attribute')
     },
-    tsx  = {
+    tsx    = {
       ['string'] = attribute_toggle('string_fragment', 'jsx_attribute'),
       ['template_string'] = attribute_toggle('template_string', 'expression_statement')
     },
-    astro = {
+    astro  = {
       ['quoted_attribute_value'] = attribute_toggle('attribute_value', 'attribute')
     },
-    html  = { ['string'] = attribute_toggle('string_fragment', 'attribute') },
+    html   = { ['string'] = attribute_toggle('string_fragment', 'attribute') },
   },
   ---@type boolean Use `dot` for repeat action
   dot_repeat = true,
   ---@type nil|function Callback for treesj error handler. func (err_text, level, ...other_text)
   on_error = nil,
 }
-
--- require 'on' ({ 'BufEnter', 'BufNew' }, )
