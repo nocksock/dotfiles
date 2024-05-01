9a1ca021d9fce85964fe5873e5abef4e7db40cfd
Nils Riedemann <nils@bleepbloop.studio>
2024-05-01 17:21:05 +0200

    feat(nvim): add php snippets

--- nvim/luasnippets/php.lua
+++ nvim/luasnippets/php.lua
@@ -15,32 +15,76 @@ local r = ls.restore_node
 local events = require('luasnip.util.events')
 local ai = require('luasnip.nodes.absolute_indexer')
 local same = function(index)
-	return f(function(arg)
-		return arg[1]
-	end, { index, index })
+  return f(function(arg)
+    return arg[1]
+  end, { index, index })
 end
+local ts_postfix = require("luasnip.extras.treesitter_postfix").treesitter_postfix
 -- }}}
 
 return {
-	s(
-		'php',
-		c(1, {
-			fmt([[<?php {} ?>]], { i(1) }),
-			fmt(
-				[=[
+  s('=', { t('<?= $'), i(1, "var"), t(' ?>') }),
+  s('if', c(1, {
+    fmt([[
+    <?php if({}): ?>
+    {}
+    <?php endif; ?>
+    ]], {
+      i(1), i(2)
+    })
+  })),
+  s('foreach', c(1, {
+    fmt(
+      [[
+      <?php foreach(${} as {}): ?>
+        {}
+      <?php endforeach; ?>
+      ]],
+      {
+        i(1),
+        c(2, {
+          fmt([[${}]], { i(1, "item") }),
+          fmt([[${} => ${}]], { i(1, "key"), i(2, "item") })
+        }),
+        i(3)
+      }
+    ),
+    fmt(
+      [[
+      foreach({} as {}) {{
+        {}
+      }}
+      ]],
+      {
+        i(1, "$items"),
+        c(2, {
+          fmt([[${}]], { i(1, "item") }),
+          fmt([[${} => ${}]], { i(1, "key"), i(2, "item") })
+        }),
+        i(3)
+      }
+    ),
+  })),
+  s(
+    'php',
+    c(1, {
+      fmt([[<?php {} ?>]], { i(1) }),
+      fmt([[<?= {} ?>]], { i(1) }),
+      fmt(
+        [=[
 					<?php
 						{}
 					?>
         ]=],
-				{ i(1) }
-			),
-		})
-	),
-	s(
-		'?',
-		c(1, {
-			fmt([[?> {} <?php]], { i(1) }),
-			fmt('?>\n{}\n<?php', { i(1) }),
-		})
-	),
+        { i(1) }
+      ),
+    })
+  ),
+  s(
+    '?',
+    c(1, {
+      fmt([[?> {} <?php]], { i(1) }),
+      fmt('?>\n{}\n<?php', { i(1) }),
+    })
+  ),
 }
