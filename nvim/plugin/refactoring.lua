9557f34841e5a152374aeea470fc6ca5d2acbf8b
Nils Riedemann <nils@bleepbloop.studio>
2025-04-27 13:39:55 +0200

    nvim: delete refactoring config (no longer using it)

--- nvim/plugin/refactoring.lua
+++
@@ -1,7 +0,0 @@
--- local bag = require 'baggage'.from {
---   'https://github.com/nvim-treesitter/nvim-treesitter',
---   "https://github.com/nvim-lua/plenary.nvim",
---   "https://github.com/ThePrimeagen/refactoring.nvim"
--- }
-
--- bag.setup("refactoring")
