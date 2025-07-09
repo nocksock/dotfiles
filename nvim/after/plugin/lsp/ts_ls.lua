aa05504ee9964c138c7139751f6028373bc36739
Nils Riedemann <nils@bleepbloop.studio>
2025-07-09 02:21:47 +0200

    nvim: ts lsp

--- nvim/after/plugin/lsp/ts_ls.lua
+++ nvim/after/plugin/lsp/ts_ls.lua
@@ -11,5 +11,8 @@ lspconfig.ts_ls.setup {
   capabilities = require "lsp-utils".capabilities,
   file_types   = { 'js', 'ts', },
   root_dir     = require('lspconfig.util').root_pattern("deno.json", "deno.jsonc", "package.json"),
-  on_attach    = require "lsp-utils".on_attach
+  on_attach    = function(client, bufnr)
+    require "lsp-utils".on_attach(client, bufnr)
+    require "twoslash-queries".attach(client, bufnr)
+  end
 }
