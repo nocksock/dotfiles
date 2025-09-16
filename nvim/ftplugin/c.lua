7f0c31e5f327847b527d48b96c0e8d712ad679d5
Nils Riedemann <nils@bleepbloop.studio>
2025-09-16 15:37:21 +0200

    nvim: use ,a to switch between c and h file in c

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -1,1 +1,7 @@
 vim.opt_local.wrap = false
+
+-- switch between alternative file
+vim.keymap.set("n", "<localleader>a", function()
+  local alt = vim.fn.expand("%:r") .. (vim.bo.filetype == "c" and ".h" or ".c")
+  vim.cmd("edit " .. alt)
+end, { buffer = true, desc = "Switch between .c and .h" })
