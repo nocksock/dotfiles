46011bffe211235104734a3e6a34fc4063e1f0ea
Nils Riedemann <nils@bleepbloop.studio>
2025-09-16 15:36:56 +0200

    nvim: add c snippets

---
+++ nvim/luasnippets/cpp.lua
@@ -0,0 +1,41 @@
+-- Helper Methods {{{
+---@diagnostic disable: unused-local, unused-function
+local ls = require('luasnip')
+local s = ls.s
+local fmt = require('luasnip.extras.fmt').fmt
+local rep = require('luasnip.extras').rep ---@diagnostic disable-line: unused-local
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require('luasnip.util.events')
+local ai = require('luasnip.nodes.absolute_indexer')
+local same = function(index)
+  return f(function(arg)
+    return arg[1]
+  end, { index, index })
+end
+-- }}}
+
+return {
+  s('hif', fmt([[
+    #ifndef {}_H
+    #define {}_H
+    
+    {}
+    
+    #endif
+  ]], {
+    f(function() 
+      return vim.fn.expand('%:t:r'):upper()
+    end),
+    f(function() 
+      return vim.fn.expand('%:t:r'):upper()
+    end),
+    i(0)
+  })),
+}
