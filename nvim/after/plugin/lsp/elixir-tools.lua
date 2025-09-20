9b9c0e4ea34784e6a642536bdd8f1909cf6ad2d6
Nils Riedemann <nils@bleepbloop.studio>
2025-09-20 11:29:23 +0200

    nvim(lsp): use expert for elixir

--- nvim/after/plugin/lsp/elixir-tools.lua
+++ nvim/after/plugin/lsp/elixir-tools.lua
@@ -1,15 +1,13 @@
-require "baggage".from { 
-  'https://github.com/elixir-tools/elixir-tools.nvim'
-}
-
 local on_attach = function(_client, bufnr)
   vim.keymap.set("n", "gd", function()
     require("fzf-lua").lsp_workspace_symbols({ query = vim.fn.expand("<cword>") })
   end, { buffer = bufnr, noremap = true })
 end
 
-require("elixir").setup({
-  nextls = { enable = false },
-  elixirls = { enable = true, on_attach = on_attach },
-  projectionist = { enable = true, on_attach = on_attach },
+vim.lsp.config('expert', {
+  cmd = { 'expert' },
+  root_markers = { 'mix.exs', '.git' },
+  filetypes = { 'elixir', 'eelixir', 'heex' },
 })
+
+vim.lsp.enable 'expert'
