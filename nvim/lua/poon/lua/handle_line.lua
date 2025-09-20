b916cdf8ae6ce81f9a60e2d9379583f6d472500b
Nils Riedemann <nils@bleepbloop.studio>
2025-09-20 11:28:42 +0200

    nvim: moving poon to external folder

--- nvim/lua/poon/lua/handle_line.lua
+++
@@ -1,7 +0,0 @@
-return function(config)
-  local config = config or R "poon.lua.config"()
-
-  return R('poon.lua.try_cmd')() 
-    or R('poon.lua.try_paths')()
-    or R('poon.lua.try_codeblock')()
-end
